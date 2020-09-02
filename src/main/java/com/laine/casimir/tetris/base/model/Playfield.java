8552e94604735e2d93377e9d006b60e66391da31
Casimir Laine <casimir.laine@gmail.com>
2020-09-03 00:09:40 +0300

    Added Playfield and some constant values.

---
+++ src/main/java/com/laine/casimir/tetris/base/model/Playfield.java
@@ -0,0 +1,8 @@
+package com.laine.casimir.tetris.base.model;
+
+public class Playfield {
+
+    public static final int WIDTH = 10;
+    public static final int HEIGHT = 40;
+    public static final int VISIBLE_HEIGHT = 20;
+}
