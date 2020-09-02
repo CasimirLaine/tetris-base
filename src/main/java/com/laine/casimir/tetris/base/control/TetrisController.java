61ebf6d35887a5aa7037ae85fe990942a8a61880
Casimir Laine <casimir.laine@gmail.com>
2020-09-03 00:11:49 +0300

    Added TetrisController with methods for game actions.

---
+++ src/main/java/com/laine/casimir/tetris/base/control/TetrisController.java
@@ -0,0 +1,36 @@
+package com.laine.casimir.tetris.base.control;
+
+public class TetrisController {
+
+    public void leftShift() {
+
+    }
+
+    public void rightShift() {
+
+    }
+
+    public void rotateClockwise() {
+
+    }
+
+    public void rotateCounterclockwise() {
+
+    }
+
+    public void hardDrop() {
+
+    }
+
+    public void softDrop() {
+
+    }
+
+    public void hold() {
+
+    }
+
+    public void pause() {
+
+    }
+}
