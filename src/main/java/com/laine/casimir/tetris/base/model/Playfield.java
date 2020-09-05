481bcba2fcfda5396af22149ff6d616d8b4b9619
Casimir Laine <casimir.laine@gmail.com>
2020-09-05 23:36:47 +0300

    Defined grid colors in Playfield.

--- src/main/java/com/laine/casimir/tetris/base/model/Playfield.java
+++ src/main/java/com/laine/casimir/tetris/base/model/Playfield.java
@@ -5,4 +5,6 @@ public class Playfield {
     public static final int WIDTH = 10;
     public static final int HEIGHT = 40;
     public static final int VISIBLE_HEIGHT = 20;
+    public static final String BACKGROUND_COLOR = "#000000";
+    public static final String GRID_COLOR = "#FFFFFF";
 }
