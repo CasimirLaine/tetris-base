ded006fdda56dddaa0a1ff84e274705e03f25df5
Casimir Laine <casimir.laine@gmail.com>
2020-12-21 23:42:43 +0200

    Added count down constant.

--- src/main/java/com/laine/casimir/tetris/base/api/TetrisConstants.java
+++ src/main/java/com/laine/casimir/tetris/base/api/TetrisConstants.java
@@ -5,6 +5,7 @@ public final class TetrisConstants {
     public static final int WIDTH = 10;
     public static final int HEIGHT = 40;
     public static final int VISIBLE_HEIGHT = 20;
+    public static final int COUNT_DOWN = 3;
 
     private TetrisConstants() {}
 }
